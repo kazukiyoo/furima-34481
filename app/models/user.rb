b3d8810fad9324bbef214670c9fc51dcb15f43b7
kazukiyoo <kazukiyoshibata@gmail.com>
2021-03-03 20:43:39 +0900

    ユーザー登録のバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,16 @@ class User < ApplicationRecord
   # validates :encrypted_password, presence: true
   validates :birthday, presence: true
 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z{6,}/.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+    validates :surname
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角文字を使用してください' } do
+    validates :surname_kana
+    validates :first_name_kana
+  end
 end
