f0e0eac6ecebff82ff98c962c17ed50d992de288
kazukiyoo <kazukiyoshibata@gmail.com>
2021-03-08 12:38:12 +0900

    商品のカテゴリ選択に関する実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,19 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :burden
+  belongs_to :days_to_delivery
+  belongs_to :prefecture
   belongs_to :user
-  has_one_attached :image
 
   validates :product_name, presence: true
   validates :price, presence: true
   validates :description, presence: true
-  validates :category_id, presence: true
-  validates :status_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :burden_id, presence: true
-  validates :days_to_delivery_id, presence: true
+  validates :status_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :burden_id, numericality: { other_than: 1 }
+  validates :days_to_delivery_id, numericality: { other_than: 1 }
+  validates :category_id, numericality: { other_than: 1 } 
 
 end
