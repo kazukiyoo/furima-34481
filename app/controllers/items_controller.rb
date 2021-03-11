38c37a2d5916f0084044e7162e65704050cbb683
kazukiyoo <kazukiyoshibata@gmail.com>
2021-03-11 11:36:34 +0900

    updateアクションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,7 +14,15 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
-    # binding.pry
+  end
+
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   def create
