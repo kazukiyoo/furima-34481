e5a0a49856c1f3f24c6bfb2ced185227acac8d79
kazukiyoo <kazukiyoshibata@gmail.com>
2021-03-11 12:19:36 +0900

    条件を満たした場合のみ編集画面に遷移できる設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:create,:new]
+  before_action :authenticate_user!, only: [:create, :new, :update, :edit]
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -14,6 +14,9 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    unless current_user.id == @item.user.id
+      redirect_to action: :index
+   end
   end
 
   def update
