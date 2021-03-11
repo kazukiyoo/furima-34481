9d1e03b403d3b616beb391799b32a9b4961f4ba0
kazukiyoo <kazukiyoshibata@gmail.com>
2021-03-11 15:44:25 +0900

    修正対応1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,31 +1,14 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:create, :new, :update, :edit]
+  before_action :item, only: [:show, :edit, :update]
+  before_action :current, only: [:edit, :update]
+
   def index
     @items = Item.all.order("created_at DESC")
   end
 
   def new
     @item = Item.new
-  end
-
-  def show
-    @item = Item.find(params[:id])
-  end
-
-  def edit
-    @item = Item.find(params[:id])
-    unless current_user.id == @item.user.id
-      redirect_to action: :index
-   end
-  end
-
-  def update
-    @item = Item.find(params[:id])
-    if @item.update(item_params)
-      redirect_to "/items/#{@item.id}"
-    else
-      render :edit
-    end
   end
 
   def create
@@ -37,8 +20,32 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to "/items/#{@item.id}"
+    else
+      render :edit
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit(:image, :product_name, :price, :description, :status_id, :prefecture_id, :burden_id, :days_to_delivery_id, :category_id).merge(user_id: current_user.id)
+  end
+
+  def item
+    @item = Item.find(params[:id])
+  end
+
+  def current
+    if current_user.id != @item.user.id
+      redirect_to action: :index
+    end
   end
 end
